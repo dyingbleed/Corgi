b317f6ee74127a809efeade587b0eebd472d0766
Anthony Li <lizhen.dgut@gmail.com>
2019-03-26 09:23:48 +0800

    feat(corgi-web): 日志优化
    Closes #26

--- corgi-web/src/main/java/com/dyingbleed/corgi/web/aop/PageControllerAop.java
+++ corgi-web/src/main/java/com/dyingbleed/corgi/web/aop/PageControllerAop.java
@@ -1,11 +1,18 @@
 package com.dyingbleed.corgi.web.aop;
 
+import org.apache.shiro.SecurityUtils;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -13,10 +20,21 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Aspect
 @Component
-public class PageControllerAop {
+public class PageControllerAOP {
+
+    private static final Logger logger = LoggerFactory.getLogger(PageControllerAOP.class);
 
     @Value("${server.display-name}")
     private String appName;
+
+    @Before("execution(org.springframework.web.servlet.ModelAndView com.dyingbleed.corgi.web.controller.page.*Controller.*(..))")
+    private void log(JoinPoint joinPoint) {
+        if (SecurityUtils.getSubject().isAuthenticated()) {
+            String principal = (String) SecurityUtils.getSubject().getPrincipal();
+            String requestURI = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getRequestURI();
+            logger.info(principal + " visit page " + requestURI);
+        }
+    }
 
     @Pointcut("execution(org.springframework.web.servlet.ModelAndView com.dyingbleed.corgi.web.controller.page.*Controller.*(..))")
     private void method() {}
